8d59937c6c08d9b111ffb658ecf8e4b9237b52f3
Abhishek Arya <abhishekarya102@gmail.com>
2024-01-06 19:28:57 +0530

    updated netlify toml and log commit script

--- log-commits.sh
+++ log-commits.sh
@@ -3,6 +3,8 @@
 # output file
 file_name="./content/changelog.md"
 
+echo "Logging commits..."
+
 # summary command
 summary_output=$(git log -n 15 --pretty=format:"%h    %cd    %s")
 
@@ -11,3 +13,5 @@ line_number=8
 
 # use awk to insert the summary output at the specified line
 awk -v line=$line_number -v text="$summary_output" 'NR==line {print text} {print}' "$file_name" > "$file_name.tmp" && mv "$file_name.tmp" "$file_name"
+
+echo "Successfully logged commits."
