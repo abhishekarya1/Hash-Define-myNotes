6e4decf62d9ad3f7e5656f37addd5ca54ac970a1
Abhishek Arya <abhishekarya102@gmail.com>
2024-01-06 16:12:53 +0530

    updated changelog logic, added gitinfo var to track latest commit

--- log-commits.sh
+++ log-commits.sh
@@ -4,18 +4,13 @@
 file_name="./content/changelog.md"
 
 # clear line range
-sed -i '8,22d' "$file_name"
+sed -i '9,22d' "$file_name"
 
 # summary command
-summary_output=$(git log -n 15 --pretty=format:"%h    %cd    %s")
+summary_output=$(git log -n 14 --pretty=format:"%h    %cd    %s")
 
 # line number to insert the output
-line_number=8
+line_number=9
 
 # use awk to insert the summary output at the specified line
 awk -v line=$line_number -v text="$summary_output" 'NR==line {print text} {print}' "$file_name" > "$file_name.tmp" && mv "$file_name.tmp" "$file_name"
-
-
-# commit changelog updates, amend previous commit
-git add content/changelog.md
-git commit --amend --no-edit
